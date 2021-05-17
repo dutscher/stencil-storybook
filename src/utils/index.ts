e2bc66d6af002d6b94911abbe43cb91d61f31524
Willy Woitas <willy.woitas@neofonie.de>
2021-05-17 12:49:13 +0200

    major update.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,6 @@ import BEM, { MODIFIER } from './tsx/bem';
 import hostClasses from './tsx/host-classes';
 import Fragment from './tsx/fragment';
 import ComponentName from './tsx/decorator-component-name';
-import getComponentNameFromFilename from '../../scripts/get-component-name-of-filename';
+import getComponentNameFromFilename from './stencil/get-component-name-of-filename';
 
 export { BEM, MODIFIER, hostClasses, Fragment, ComponentName, getComponentNameFromFilename };
